61aef4a81fc5cf2caea463616827d352dd3cacc1
Srikanth K <kolleparasrikanth@gmail.com>
2024-02-27 23:05:18 +0530

    formatted the files

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 import express from "express";
-import productRouter from "./src/features/product/product.routes.js"
+import productRouter from "./src/features/product/product.routes.js";
 
 const server = express();
-
 
 server.use("/api/products", productRouter);
 
 server.get("/", (req, res) => {
-    res.send("Welcome to E-Commerce APIs")
-})
+  res.send("Welcome to E-Commerce APIs");
+});
 
-server.listen(3200, ()=>{
-    console.log('Server is listening in 3200'); 
+server.listen(3200, () => {
+  console.log("Server is listening in 3200");
 });
--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -1,12 +1,10 @@
-import express from 'express';
-import ProductController from './product.controller.js';
+import express from "express";
+import ProductController from "./product.controller.js";
 
 const productRouter = express.Router();
 const productController = new ProductController();
 
 productRouter.get("/", productController.getAllProducts);
-productRouter.post("/",productController.addProduct);
-
-
+productRouter.post("/", productController.addProduct);
 
 export default productRouter;
