84cc301eebbcdadc0f5ce312d81625856fe6ca08
Srikanth K <kolleparasrikanth@gmail.com>
2024-02-28 13:18:31 +0530

    fetch one product

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -3,7 +3,7 @@ import ProductModel from "./product.model.js";
 export default class ProductController{
 
     getAllProducts(req,res){
-        const products = ProductModel.GetAll();
+        const products = ProductModel.getAll();
         res.status(200).send(products);
     }
 
@@ -21,6 +21,15 @@ export default class ProductController{
 
     rateProduct(req,res){}
 
-    getOneProduct(req,res){}
+    getOneProduct(req,res){
+        const id = req.params.id;
+        const product= ProductModel.getOne(id);
+        if(!product){
+            res.status(404).send('Product not found')
+        }
+        else{
+            res.status(200).send(product);
+        }
+    }
 
 }
--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -1,24 +1,24 @@
 // Manage routes/paths to ProductController
 
 // 1. Import express.
-import express from 'express';
-import ProductController from './product.controller.js';
-import {upload} from '../../middlewares/fileupload.middleware.js';
+import express from "express";
+import ProductController from "./product.controller.js";
+import { upload } from "../../middlewares/fileupload.middleware.js";
 
 // 2. Initialize Express router.
 const productRouter = express.Router();
 const productController = new ProductController();
 
 // All the paths to the controller methods.
-// localhost/api/products 
-productRouter.get(
-    '/', 
-productController.getAllProducts
-);
-productRouter.post(
-    '/', 
-upload.single('imageUrl'), 
-productController.addProduct);
+// localhost/api/products
+productRouter.get("/", productController.getAllProducts);
 
+productRouter.post(
+  "/",
+  upload.single("imageUrl"),
+  productController.addProduct
+);
+
+productRouter.get("/:id", productController.getOneProduct);
 
 export default productRouter;
