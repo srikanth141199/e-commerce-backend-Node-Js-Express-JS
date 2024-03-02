ba3a7092ba3c20970a25280922c8f7131985e31f
Srikanth K <kolleparasrikanth@gmail.com>
2024-03-02 22:12:12 +0530

    log lib winston implementation

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,20 +1,30 @@
 import fs from "fs";
+import winston from "winston";
 
 const fsPromise = fs.promises;
 
-async function log(logData) {
-  try {
-    logData = `\n ${new Date().toString()}-${logData}`;
-    await fsPromise.appendFile("log.txt", logData);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function log(logData) {
+//   try {
+//     logData = `\n ${new Date().toString()}-${logData}`;
+//     await fsPromise.appendFile("log.txt", logData);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+const logger = winston.createLogger({
+    level:'info',
+    format:winston.format.json(),
+    defaultMeta:{service: 'request-logging'},
+    transports:[
+        new winston.transports.File({filename:"logs.txt"})
+    ]
+})
 
 const loggerMiddleware = async (req, res, next) => {
   if (!req.url.includes("signIn")) {
     const logData = `${req.url}-${JSON.stringify(req.body)}`;
-    await log(logData);
+    logger.info(logData);
   }
   next();
 };
